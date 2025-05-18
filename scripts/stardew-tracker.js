37446f4e65c8f408b21f3e1babd2eec2cac345ed
Brynn <lavenderbd@appstate.edu>
2025-05-18 15:44:16 -0400

    Login button in header now works and user can toggle between login and register divs

--- scripts/stardew-tracker.js
+++ scripts/stardew-tracker.js
@@ -1,7 +1,18 @@
-document.getElementById("login").addEventListener("click", showLogin());
-document.getElementById("login-here").addEventListener("click", showLogin());
+// Stardew Valley Tracker JavaScript
 
-function showLogin() {
+// Function to toggle the visibility of the login and register forms
+
+document.getElementById("login").addEventListener("click", function() {
     document.querySelector(".login").style.display = "block";
     document.querySelector(".register").style.display = "none";
-}
+});
+
+document.getElementById("login-here").addEventListener("click", function() {
+    document.querySelector(".login").style.display = "block";
+    document.querySelector(".register").style.display = "none";
+});
+
+document.getElementById("register-here").addEventListener("click", function() {
+    document.querySelector(".login").style.display = "none";
+    document.querySelector(".register").style.display = "block";
+});
