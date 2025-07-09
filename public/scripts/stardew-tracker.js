f203ab345bd9340973feea09e16f64272e5e85c5
Brynn <lavenderbd@appstate.edu>
2025-07-09 16:44:45 -0400

    Worked on checklist frontend

--- public/scripts/stardew-tracker.js
+++ public/scripts/stardew-tracker.js
@@ -6,6 +6,7 @@ document.getElementById("login").addEventListener("click", function() {
     document.querySelector(".login").style.display = "block";
     document.querySelector(".register").style.display = "none";
     document.querySelector(".auth-status").style.display = "none";
+    hideChecklists();
 });
 
 document.getElementById("login-here").addEventListener("click", function() {
@@ -22,11 +23,61 @@ document.getElementById("register-here").addEventListener("click", function() {
 
 document.querySelectorAll("ion-icon[name=close-outline]").forEach(function(icon) {
     icon.addEventListener("click", function() {
-        document.querySelector(".login").style.display = "none";
-        document.querySelector(".register").style.display = "none";
-        document.querySelector(".auth-status").style.display = "none";
-        console.log("View closed");
+        hideLoginRegisterAuth();
+        hideChecklists();
     });
+});
+
+function hideLoginRegisterAuth() {
+    document.querySelector(".login").style.display = "none";
+    document.querySelector(".register").style.display = "none";
+    document.querySelector(".auth-status").style.display = "none";
+    document.querySelector(".wrong-email-password").innerHTML = ``;
+    document.querySelector(".terms-unchecked").innerHTML = ``;
+}
+
+function hideChecklists() {
+    document.querySelector(".spring-checklist").style.display = "none";
+    document.querySelector(".summer-checklist").style.display = "none";
+    document.querySelector(".fall-checklist").style.display = "none";
+    document.querySelector(".winter-checklist").style.display = "none";
+    console.log("All checklists hidden");
+}
+
+document.getElementById("spring-btn").addEventListener("click", function() {
+    document.querySelector(".spring-checklist").style.display = "block";
+    document.querySelector(".summer-checklist").style.display = "none";
+    document.querySelector(".fall-checklist").style.display = "none";
+    document.querySelector(".winter-checklist").style.display = "none";
+    console.log("Spring season selected");
+    hideLoginRegisterAuth();
+});
+
+document.getElementById("summer-btn").addEventListener("click", function() {
+    document.querySelector(".spring-checklist").style.display = "none";
+    document.querySelector(".summer-checklist").style.display = "block";
+    document.querySelector(".fall-checklist").style.display = "none";
+    document.querySelector(".winter-checklist").style.display = "none";
+    console.log("Summer season selected");
+    hideLoginRegisterAuth();
+});
+
+document.getElementById("fall-btn").addEventListener("click", function() {
+    document.querySelector(".spring-checklist").style.display = "none";
+    document.querySelector(".summer-checklist").style.display = "none";
+    document.querySelector(".fall-checklist").style.display = "block";
+    document.querySelector(".winter-checklist").style.display = "none";
+    console.log("Fall season selected");
+    hideLoginRegisterAuth();
+});
+
+document.getElementById("winter-btn").addEventListener("click", function() {
+    document.querySelector(".spring-checklist").style.display = "none";
+    document.querySelector(".summer-checklist").style.display = "none";
+    document.querySelector(".fall-checklist").style.display = "none";
+    document.querySelector(".winter-checklist").style.display = "block";
+    console.log("Winter season selected");
+    hideLoginRegisterAuth();
 });
 
 
