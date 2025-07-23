2cdb0cf492da5307c3a27cdb64571799ebace30a
Brynn <lavenderbd@appstate.edu>
2025-07-23 09:53:36 -0400

    Started work on server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const mongodb = require('mongodb');  // on GitHub it is { ServerDescriptionChangedEvent }
 
 const connectionString = 'mongodb+srv://lavenderbd:8RlV8tczen0teUF0@stardewtracker.eqmhakf.mongodb.net/?retryWrites=true&w=majority&appName=StardewTracker';
 const PORT = 5002;
@@ -12,5 +11,33 @@ app.use(cors());
 app.use(express.json());
 
 const path = require('path');
+const { type } = require('os');
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('MongoDB connection error:', err);
+});
+
+const itemSchema = new mongoose.Schema({
+    name: String,
+    imageUrl: String,
+    additionalInfo: String,
+    season: [String],
+    userId: String,
+    checked: { type: Boolean, default: false }
+});
+
+const BundleItem = mongoose.model('bundleItem', itemSchema);
+
+app.get('/api/items/:season', async (req, res) => {
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
